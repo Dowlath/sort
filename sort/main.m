f8f67fc20244c919d1db684cfdab27367abc2a3c
Dowlath <hibrise@IKRAMs-Mac-mini.local>
2014-08-19 11:19:18 +0530

    Initial Commit

---
+++ sort/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  sort
+//
+//  Created by Hibrise on 19/08/14.
+//  Copyright (c) 2014 Hibrise. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
